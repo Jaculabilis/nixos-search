3dfac437b3ed5db437d4853e785be18ab895a7b4
Tim Van Baak <tim.vanbaak@gmail.com>
2025-05-15 20:07:53 -0700

    Basic reactivity

--- frontend-ng/src/App.vue
+++ frontend-ng/src/App.vue
@@ -1,26 +1,39 @@
 <script setup lang="ts">
+import { ref, type Ref } from 'vue';
+
+const query = ref('');
+const channel = ref('24.11');
+
+const queries: Ref<string[], string[]> = ref([])
+function submit() {
+  queries.value.push(query.value);
+}
 </script>
 
 <template>
   <main>
     <h1>Search more than <strong>2 packages</strong></h1>
 
-    <form>
+    <form @submit.prevent="submit">
       <div style="display: grid; grid-template-columns: auto 8em;">
         <input
           id="search-query-input"
           type="text"
           autofocus
           placeholder="Search for packages"
+          v-model="query"
         />
         <button type="submit">Search</button>
       </div>
       Channel:
-      <input type="radio" id="24.11" name="channel" value="24.11" checked />
+      <input type="radio" id="24.11" value="24.11" v-model="channel" />
       <label for="24.11">24.11</label>
-      <input type="radio" id="unstable" name="channel" value="unstable" />
+      <input type="radio" id="unstable" value="unstable" v-model="channel" />
       <label for="unstable">Unstable</label>
     </form>
+    <p>Current query: {{ query }}</p>
+    <p>Current channel: {{ channel }}</p>
+    <p>Queries: {{ queries }}</p>
   </main>
 </template>
 
